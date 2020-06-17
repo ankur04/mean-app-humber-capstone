5976b3c8ca2074bc0a04c3bbf90cfa68692baa3b
ankur04 <ankurpravinpatel@gmail.com>
2020-06-17 16:48:24 -0400

    test

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.db, {
    useNewUrlParser: true
 }).then(() => {
-      console.log('Database sucessfully connected')
-   },
+   console.log('Database sucessfully connected')
+},
    error => {
       console.log('Database could not connected: ' + error)
    }
@@ -24,7 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: false
 }));
-app.use(cors()); 
+// var distDir = __dirname + "/dist/";
+// app.use(express.static(distDir));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
 app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
 app.use('/api', employeeRoute)
@@ -32,7 +34,7 @@ app.use('/api', employeeRoute)
 // Create port
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
+   console.log('Connected to port ' + port)
 })
 
 // Find 404 and hand over to error handler
@@ -42,7 +44,7 @@ app.use((req, res, next) => {
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.error(err.message); // Log error message in our server's console
-  if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
-  res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
+   console.error(err.message); // Log error message in our server's console
+   if (!err.statusCode) err.statusCode = 500; // If err has no specified error code, set error code to 'Internal Server Error (500)'
+   res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
