903dff882a69d405e5f835725591c09cde9a3e45
dharmesh95 <95dharmesh@gmail.com>
2020-07-01 18:23:48 -0400

    created journey card

--- src/app/components/home/journey/journey.component.ts
+++ src/app/components/home/journey/journey.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-journey',
   templateUrl: './journey.component.html',
-  styleUrls: ['./journey.component.css']
+  styleUrls: ['./journey.component.css', './../../../styles/form.css']
 })
 export class JourneyComponent implements OnInit {
+
+  stepData = "This step focuses on prototyping your first product. It has one key skill, and two canvasses."
+  nextData = "This step focuses on designing the product to meet the key needs of your users. It has one key skill, and two canvasses."
 
   constructor() { }
 
